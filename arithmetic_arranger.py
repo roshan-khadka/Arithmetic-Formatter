b4b9b4ba2ae32c1f745fd51da04254a145815148
Roshan Khadka <65776857+roshan-khadka@users.noreply.github.com>
2022-01-21 12:57:22 +0200

    Update arithmetic_arranger.py

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,1 +1,25 @@
-
+def arithmetic_arranger(problems):
+  
+  # Starting by creating list of all the acceptable operators
+  operators = []
+  
+  if len(problems) > 5:
+    return "Error: Too many problems."
+ 
+  for problem in range(len(problems)):
+    if "+" in problems[problem]:
+      operators.append("+")
+     elif "-" in problems[problem]:
+      operators.append("-")
+     else:
+      return "Error: Operator must be + or -."
+    
+    # Next create a list of num_1 and num_2 
+    num1 = []
+    num2 = []
+    
+    
+  
+  
+  
+  
